a81658871cc0ab8e20dd245e4f2d23646e3252b9
riagopu <59451668+riagopu@users.noreply.github.com>
2021-12-01 15:45:32 -0800

    added sign in & log in buttons

--- src/App.js
+++ src/App.js
@@ -23,25 +23,101 @@ const firebaseConfig = {
     appId: "1:1050666858786:web:7cbb73999edafbcbbfcd7b",
     measurementId: "G-0BBJ256RS9"
 };
-console.log("before initialize")
-firebase.initializeApp(firebaseConfig);
-console.log("after initialize")
-const db = firebase.firestore();
 
-const collectionName = "waverlywang7-listitems";
-const collectionOfLists = db.collection(collectionName);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 const auth = firebase.auth();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 
+const collectionName = "waverlywang7-listitems";
+
 function App(props) {
+
+    const [user, loading, error] = useAuthState(auth);
+
+    function verifyEmail() {
+        auth.currentUser.sendEmailVerification();
+    }
+
+    if (loading) {
+        return <p>Checking...</p>;
+    } else if (user) {
+        return <div>
+            {user.displayName || user.email}
+            <SignedInApp {...props} user={user}/>
+            <button type="button" onClick={() => auth.signOut()}>Logout</button>
+            {!user.emailVerified && <button type="button" onClick={verifyEmail}>Verify email</button>}
+        </div>
+    } else {
+        return <>
+            {error && <p>Error App: {error.message}</p>}
+            <button>
+                <SignIn key="Sign In"/>
+                <SignUp key="Sign Up"/>
+            </button>
+        </>
+    }
+
+};
+
+const FAKE_EMAIL = 'foo@bar.com';
+const FAKE_PASSWORD = 'xyzzyxx';
+
+function SignIn() {
+    const [
+        signInWithEmailAndPassword,
+        userCredential, loading, error
+    ] = useSignInWithEmailAndPassword(auth);
+
+    if (userCredential) {
+        // Shouldn't happen because App should see that
+        // we are signed in.
+        return <div>Unexpectedly signed in already</div>
+    } else if (loading) {
+        return <p>Logging in…</p>
+    }
+    return <div>
+        {error && <p>"Error logging in: " {error.message}</p>}
+        <button onClick={() =>
+            signInWithEmailAndPassword(FAKE_EMAIL, FAKE_PASSWORD)}>Login with test user Email/PW
+        </button>
+        <button onClick={() =>
+            auth.signInWithPopup(googleProvider)}>Login with Google
+        </button>
+    </div>
+}
+
+function SignUp() {
+    const [
+        createUserWithEmailAndPassword,
+        userCredential, loading, error
+    ] = useCreateUserWithEmailAndPassword(auth);
+
+    if (userCredential) {
+        // Shouldn't happen because App should see that
+        // we are signed in.
+        return <div>Unexpectedly signed in already</div>
+    } else if (loading) {
+        return <p>Signing up…</p>
+    }
+    return <div>
+        {error && <p>"Error signing up: " {error.message}</p>}
+        <button onClick={() =>
+            createUserWithEmailAndPassword(FAKE_EMAIL, FAKE_PASSWORD)}>
+            Create test user
+        </button>
+
+    </div>
+}
+
+
+
+function SignedInApp(props) {
+    const collectionOfLists = db.collection(collectionName);
+
     const [selectedListId, setSelectedListId] = useState(null);
     const [currentListName, setCurrentListName] = useState(null);
     const [value, loading, error] = useCollection(collectionOfLists); // let MyList and myLists handle
-    // const [user, loading, error] = useAuthState(auth);
-    //
-    // function verifyEmail() {
-    //     auth.currentUser.sendEmailVerification();
-    // }
 
     let data = null;
     if (value !== undefined) {
@@ -99,81 +175,9 @@ function App(props) {
         setCurrentListName(null);
     }
 
-    // return <div>
-    //     {selectedListId ? <MyList
-    //             name={currentListName}
-    //             listId={selectedListId}
-    //             returnHome={returnHome}
-    //             onListDeleted={handleDeleteList}
-    //             onItemAdded={handleItemAdded}
-    //             onListItemFieldChanged={handleListItemFieldChanged}
-    //         /> :
-    //         <MyLists
-    //             setListIdAndName={setListIdAndName}
-    //             list={data}
-    //             onListAdded={handleAddList}
-    //         />
-    //     }
-    // </div>;
-// };
-//
-//
-//
-// function SignUp() {
-//     const [
-//         createUserWithEmailAndPassword,
-//         userCredential, loading, error
-//     ] = useCreateUserWithEmailAndPassword(auth);
-//
-//     if (userCredential) {
-//         // Shouldn't happen because App should see that
-//         // we are signed in.
-//         return <div>Unexpectedly signed in already</div>
-//     } else if (loading) {
-//         return <p>Signing up…</p>
-//     }
-//     return <div>
-//         {error && <p>"Error signing up: " {error.message}</p>}
-//         <button onClick={() =>
-//             createUserWithEmailAndPassword(FAKE_EMAIL, FAKE_PASSWORD)}>
-//             Create test user
-//         </button>
-//
-//     </div>
-// }
-//
-// const FAKE_EMAIL = 'foo@bar.com';
-// const FAKE_PASSWORD = 'xyzzyxx';
-//
-// function SignIn() {
-//     const [
-//         signInWithEmailAndPassword,
-//         userCredential, loading, error
-//     ] = useSignInWithEmailAndPassword(auth);
-//
-//     if (userCredential) {
-//         // Shouldn't happen because App should see that
-//         // we are signed in.
-//         return <div>Unexpectedly signed in already</div>
-//     } else if (loading) {
-//         return <p>Logging in…</p>
-//     }
-//     return <div>
-//         {error && <p>"Error logging in: " {error.message}</p>}
-//         <button onClick={() =>
-//             signInWithEmailAndPassword(FAKE_EMAIL, FAKE_PASSWORD)}>Login with test user Email/PW
-//         </button>
-//         <button onClick={() =>
-//             auth.signInWithPopup(googleProvider)}>Login with Google
-//         </button>
-//     </div>
-// }
-//
-// function SignedInApp(props) {
-//
-
     return <div>
-        {selectedListId ? <MyList
+        {loading && <h1>Loading</h1>}
+        {data && selectedListId ? <MyList
                 db={db}
                 name={currentListName}
                 listId={selectedListId}
